33356f208a63fe1fea9d7f47bdccda5bcbf3d82a
_TrueLecter_ <andrew.panassiouk@gmail.com>
2017-07-19 08:11:47 +0300

    Created class ChildAddRemoveObserver

--- src/main.js
+++ src/main.js
@@ -1,36 +1,35 @@
 const Constants = require('./constants.js')
 const Picker = require('./picker.js')
+const Observer = require('./observer.js').ChildAddRemoveObserver
 
 const initEmojis = require('./initializer.js')
 
-function watchForEmojiPickerChange (listener) {
-  const observer = new MutationObserver(mutations => {
-    if (typeof listener === 'function') {
-      listener(mutations)
-    }
-  })
-  observer.observe($(Constants.EMOJI_PICKER_PATH)[0], { childList: true })
-  return observer
+function attachPickerObserver () {
+  if (window.better_emojis.pickerObserver) {
+    window.better_emojis.pickerObserver.disconnect()
+  } else {
+    window.better_emojis.pickerObserver = new Observer(null,
+      () => {
+        if ($(Constants.EMOJI_PICKER_PATH).find('.emoji-picker').length &&
+                      ($('.channel-textarea-emoji').hasClass('popout-open') || $('.btn-reaction.popout-open').length)) {
+          Picker.show()
+        }
+      },
+      () => {
+        if (window.better_emojis.current_cluster) {
+          window.better_emojis.current_cluster.destroy()
+        }
+      }
+    )
+  }
+  window.better_emojis.pickerObserver.reattach = attachPickerObserver
+  window.better_emojis.pickerObserver.observe($(Constants.EMOJI_PICKER_PATH)[0])
 }
 
 initEmojis().then((spanCache) => {
   Picker.setCommonEmojiSpanCache(spanCache.spanCache)
-  console.log('Better Emojis initialized')
   setTimeout(() => {
-    window.better_emojis.observer = watchForEmojiPickerChange(([mutation]) => {
-      if (mutation.type === 'childList') {
-        if (mutation.addedNodes.length > 0) {
-          if ($(Constants.EMOJI_PICKER_PATH).find('.emoji-picker').length &&
-                    ($('.channel-textarea-emoji').hasClass('popout-open') || $('.btn-reaction.popout-open').length)) {
-            Picker.show()
-          }
-        }
-        if (mutation.removedNodes.length) {
-          if (window.better_emojis.current_cluster) {
-            window.better_emojis.current_cluster.destroy()
-          }
-        }
-      }
-    })
+    attachPickerObserver()
+    console.log('Better Emojis initialized')
   }, 2000)
 })
---
+++ src/observer.js
@@ -0,0 +1,86 @@
+const observer = Symbol('observer')
+const addListeners = Symbol('addListeners')
+const removeListeners = Symbol('removeListeners')
+
+class ChildAddRemoveObserver {
+  constructor (target = null, addListener = null, removeListener = null) {
+    this[observer] = new MutationObserver(([mutation]) => {
+      if (mutation.type === 'childList') {
+        if (mutation.addedNodes.length > 0) {
+          for (const listener of this[addListeners]) {
+            if (typeof listener === 'function') {
+              listener(mutation)
+            }
+          }
+        }
+
+        if (mutation.removedNodes.length > 0) {
+          for (const listener of this[removeListeners]) {
+            if (typeof listener === 'function') {
+              listener(mutation)
+            }
+          }
+        }
+      }
+    })
+    this[addListeners] = []
+    this[removeListeners] = []
+
+    if (target) {
+      this.observe(target)
+    }
+
+    this.add(addListener).remove(removeListener)
+  }
+
+  observe (target) {
+    if (!target || !(target instanceof Node)) {
+      throw new TypeError('Target must be Node!')
+    }
+
+    this[observer].observe(target, { childList: true })
+
+    return this
+  }
+
+  add (listener) {
+    return this.on('add', listener)
+  }
+
+  remove (listener) {
+    return this.on('remove', listener)
+  }
+
+  on (evt, listener) {
+    if (typeof listener === 'function') {
+      switch (evt) {
+        case 'add':
+          this[addListeners].push(listener)
+          break
+        case 'remove':
+          this[removeListeners].push(listener)
+          break
+      }
+    }
+    return this
+  }
+
+  off (evt) {
+    switch (evt) {
+      case 'add':
+        this[addListeners].length = 0
+        break
+      case 'remove':
+        this[removeListeners].length = 0
+        break
+    }
+    return this
+  }
+
+  disconnect () {
+    this[observer].disconnect()
+    return this
+  }
+}
+
+module.exports.ChildAddRemoveObserver = ChildAddRemoveObserver
