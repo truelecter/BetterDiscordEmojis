4c0daaf89f8cde4dae49bd619e70adc253052efe
_TrueLecter_ <andrew.panassiouk@gmail.com>
2017-07-19 20:01:08 +0300

    Hotfix on error with function type check

--- src/observer.js
+++ src/observer.js
@@ -55,7 +55,7 @@ class ChildAddRemoveObserver {
 	}
 
 	on(evt, listener) {
-		if (typeof listener === 'function') {
+		if (typeof listener !== 'function') {
 			throw new TypeError('Listener must be function');
 		}
 
