9639aa2647a62dc9080a185bc6c999ef293d423b
_TrueLecter_ <andrew.panassiouk@gmail.com>
2017-07-28 13:18:47 +0300

    Updated class contaners numbers

--- src/classes.js
+++ src/classes.js
@@ -1,14 +1,14 @@
 'use strict';
 
-exports.SETTINGS_CLASSES = n(2142);
-exports.SWITCH_CLASSES = n(2140);
-exports.FLEX_CHILD_CLASSES = n(2124);
-exports.FLEX_CLASSES = n(2105);
-exports.HEADER_CLASSES = n(2151);
-exports.SWITCH_ITEM_CLASSES = n(2141);
-exports.DIVIDER_ITEM_CLASSES = n(2147);
-exports.LABEL_ITEM_CLASSES = n(2150);
-exports.FONT_SIZE_CLASSES = n(2143);
+exports.SETTINGS_CLASSES = n(2392);
+exports.SWITCH_CLASSES = n(2390);
+exports.FLEX_CHILD_CLASSES = n(2374);
+exports.FLEX_CLASSES = n(2355);
+exports.HEADER_CLASSES = n(2401);
+exports.SWITCH_ITEM_CLASSES = n(2391);
+exports.DIVIDER_ITEM_CLASSES = n(2397);
+exports.LABEL_ITEM_CLASSES = n(2400);
+exports.FONT_SIZE_CLASSES = n(2393);
 
 exports.SIDEBAR_BUTTON_CLASS = 'be-settings-button';
 
