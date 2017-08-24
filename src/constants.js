9ab96d141ae90446f0079209d03536dd54ee1bf7
_TrueLecter_ <andrew.panassiouk@gmail.com>
2017-08-24 22:05:01 +0300

    Updated module numbers

--- src/constants.js
+++ src/constants.js
@@ -7,15 +7,15 @@ exports.API_BASE = 'https://discordapp.com/api';
 exports.EMOJI_PICKER_PATH = '#app-mount > div > div:nth-child(7)';
 exports.EMOJI_BUTTON_CLASS = n(2161).emojiButton;
 exports.CHANNEL_TEXTAREA_CLASS = n(2161).channelTextArea;
-exports.LOCAL_STORAGE_MODULE = n(1625); // impl
-exports.EMOJI_STORAGE_MODULE = n(176).default;
-exports.STANDART_EMOJI_CLASS = n(176).Emoji;
+exports.LOCAL_STORAGE_MODULE = n(1624); // impl
+exports.EMOJI_STORAGE_MODULE = n(175).default;
+exports.STANDART_EMOJI_CLASS = n(175).Emoji;
 exports.SERVERS_STORAGE_MODULE = n(13); // getGuilds
 exports.SERVERS_PERMISSIONS_MODULE = n(56); // getGuildPermissions
 exports.TRANSLATION_MODULE = n(3); // Messages
 exports.CUSTOM_EMOJI_STORAGE_MODULE = n(199); // getDisambiguatedEmojiContext
 exports.TOKEN_KEY = n(0).TOKEN_KEY;
-exports.REACTION_EMOJI_CONVERTER = n(324); // toReactionEmoji
+exports.REACTION_EMOJI_CONVERTER = n(325); // toReactionEmoji
 /* May be changed with discord updates.END */
 
 exports.EMOJI_ROW_CATEGORY_HEIGHT = 32;
