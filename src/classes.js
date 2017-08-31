66b544b87bb5b76a5f5d1fdc3392ae2a633aba0b
_TrueLecter_ <andrew.panassiouk@gmail.com>
2017-08-31 11:16:36 +0300

    Updated modules numbers

--- src/classes.js
+++ src/classes.js
@@ -1,15 +1,15 @@
 'use strict';
 
-exports.SETTINGS_CLASSES = n(2188);
-exports.SWITCH_CLASSES = n(2186);
-exports.FLEX_CHILD_CLASSES = n(2168);
-exports.FLEX_CLASSES = n(2150);
-exports.HEADER_CLASSES = n(2197);
-exports.SWITCH_ITEM_CLASSES = n(2187);
-exports.DIVIDER_ITEM_CLASSES = n(2193);
-exports.LABEL_ITEM_CLASSES = n(2196);
-exports.FONT_SIZE_CLASSES = n(2189);
-exports.CARD_CLASSES = n(2160);
+exports.SETTINGS_CLASSES = n(2196);
+exports.SWITCH_CLASSES = n(2194);
+exports.FLEX_CHILD_CLASSES = n(2175);
+exports.FLEX_CLASSES = n(2157);
+exports.HEADER_CLASSES = n(2205);
+exports.SWITCH_ITEM_CLASSES = n(2195);
+exports.DIVIDER_ITEM_CLASSES = n(2201);
+exports.LABEL_ITEM_CLASSES = n(2204);
+exports.FONT_SIZE_CLASSES = n(2197);
+exports.CARD_CLASSES = n(2167);
 
 exports.SIDEBAR_BUTTON_CLASS = 'be-settings-button';
 exports.SERVER_CARD_CLASSES = {
--- src/constants.js
+++ src/constants.js
@@ -5,15 +5,15 @@ exports.API_BASE = 'https://discordapp.com/api';
 
 /* May be changed with discord updates */
 exports.EMOJI_PICKER_PATH = '#app-mount > div > div:nth-child(7)';
-exports.EMOJI_BUTTON_CLASS = n(2161).emojiButton;
-exports.CHANNEL_TEXTAREA_CLASS = n(2161).channelTextArea;
-exports.LOCAL_STORAGE_MODULE = n(1624); // impl
+exports.EMOJI_BUTTON_CLASS = n(2168).emojiButton;
+exports.CHANNEL_TEXTAREA_CLASS = n(2168).channelTextArea;
+exports.LOCAL_STORAGE_MODULE = n(1627); // impl
 exports.EMOJI_STORAGE_MODULE = n(175).default;
 exports.STANDART_EMOJI_CLASS = n(175).Emoji;
 exports.SERVERS_STORAGE_MODULE = n(13); // getGuilds
 exports.SERVERS_PERMISSIONS_MODULE = n(56); // getGuildPermissions
 exports.TRANSLATION_MODULE = n(3); // Messages
-exports.CUSTOM_EMOJI_STORAGE_MODULE = n(199); // getDisambiguatedEmojiContext
+exports.CUSTOM_EMOJI_STORAGE_MODULE = n(202); // getDisambiguatedEmojiContext
 exports.TOKEN_KEY = n(0).TOKEN_KEY;
 exports.REACTION_EMOJI_CONVERTER = n(325); // toReactionEmoji
 /* May be changed with discord updates.END */
